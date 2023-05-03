3259be23a4742701ddeefd0890291315717aa934
prakriti5dhang <prakritidhang@gmail.com>
2023-05-03 16:45:37 +0530

    activity diagram

--- experiment/simulation/js/main.js
+++ experiment/simulation/js/main.js
@@ -56,7 +56,13 @@ function viewsolutionb(){ //submit button
   document.getElementById("show_result2").style.display="none";
     document.getElementById("viewsol").disabled=false;
     document.getElementById("dispviewbtn").disabled=true;
+    if(ex.options[ex.selectedIndex].value==1){
     window.scrollBy(0, 1800);
+    }
+    if(ex.options[ex.selectedIndex].value==2){
+      window.scrollBy(0, 2800);
+    }
+
 }
 
 function solview(){  //view solution button
@@ -64,11 +70,13 @@ function solview(){  //view solution button
   document.getElementById("show_result1").style.display="block";
   document.getElementById("show_result2").style.display="none";
     document.getElementById("viewsol").disabled=true;
+    
   }
   if(ex.options[ex.selectedIndex].value==2){
     document.getElementById("show_result2").style.display="block";
     document.getElementById("show_result1").style.display="none";
       document.getElementById("viewsol").disabled=true;
+      
     }
 
 }
